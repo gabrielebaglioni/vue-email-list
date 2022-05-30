f402065ccd0c4e3d58b7961514f2deb11383ff2d
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-30 15:37:30 +0200

    done

--- main.js
+++ main.js
@@ -4,4 +4,12 @@ const app = new Vue({
         emails: [],
     },
     methods: {},
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((elm) => {
+                    this.emails.push(elm.data.response)
+                })
+        }
+    }
 })
